3e297624fdea71df9a7109d675f8418096916b70
damianheredia90 <damianheredia90@gmail.com>
2024-01-10 17:11:55 +0000

    esto es mi primer commit en idx

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Pruebita from "./components/Pruebita"
+
+function App() {
+  return(
+    <div style={{backgroundColor:"green"}}>
+      <h1>hola esto es esto </h1>
+      <Pruebita 
+      size= "40px"
+      
+      />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Pruebita.jsx
@@ -0,0 +1,9 @@
+
+
+const Pruebita = (props) => {
+    return(
+        <h2 style={{fontSize: props.size}} >ESTO ES UNA PRUEBITA</h2>
+    )
+}
+
+export default Pruebita;
